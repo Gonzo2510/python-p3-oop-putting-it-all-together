49155d984bafbccbd13405750da4f753fe07f5b9
Jacob Gonzales <jagonz@live.com>
2023-11-01 17:33:11 -0500

    complete lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title="My Book Title", page_count = 1):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+    @page_count.setter
+    def page_count(self, page_count):
+        if type(page_count) != int:
+            print("page_count must be an integer")
+        else:
+            self._page_count = page_count
+
+    @property
+    def title(self):
+        return self._title
+    @title.setter
+    def title(self, title):
+        self._title = title
+
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
     
         
